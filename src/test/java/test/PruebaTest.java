4ba0905a7bbec07c8754e0d50fb4c3a343ed4b3c
JavierCamposCuesta <javiercamposcuesta@gmail.com>
2022-02-10 21:56:01 +0100

    f

--- src/test/java/test/PruebaTest.java
+++ src/test/java/test/PruebaTest.java
@@ -1,27 +1,27 @@
-package test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = com.example.demo.CarritoCompraSpringApplication.class)
-@ExtendWith(SpringExtension.class)
-class PruebaTest {
-
-	@Test
-	void test1() {
-		assertTrue(true);
-	}
-	
-	@Test
-	void test2() {
-		assertTrue(true);
-	}
-
-}
+//package test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = com.example.demo.CarritoCompraSpringApplication.class)
+//@ExtendWith(SpringExtension.class)
+//class PruebaTest {
+//
+//	@Test
+//	void test1() {
+//		assertTrue(true);
+//	}
+//	
+//	@Test
+//	void test2() {
+//		assertTrue(true);
+//	}
+//
+//}
