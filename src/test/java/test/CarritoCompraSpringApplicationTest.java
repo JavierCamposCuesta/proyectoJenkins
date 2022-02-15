0501d88c56d941e51b1e8abfa043f8aa3fcd4a8a
JavierCamposCuesta <javiercamposcuesta@gmail.com>
2022-02-15 10:52:09 +0100

    ds

--- src/test/java/test/CarritoCompraSpringApplicationTest.java
+++ src/test/java/test/CarritoCompraSpringApplicationTest.java
@@ -5,31 +5,25 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import com.example.demo.model.Pedido;
 import com.example.demo.model.Usuario;
 import com.example.demo.service.PedidoService;
 import com.example.demo.service.UsuarioService;
 
+@ContextConfiguration
 @SpringBootTest(classes = com.example.demo.CarritoCompraSpringApplication.class)
 class CarritoCompraSpringApplicationTest {
 
-	@Autowired
-	private PedidoService pedidoService;
 	
-	@Autowired
-	private UsuarioService usuarioService;
 	
-	@Test
-	void contextLoads() {
-		
-	}
+	
 	
 	@Test
 	void prueba() {
-		Usuario usuario = new Usuario();
-		usuarioService.add(usuario);
-		assertEquals(5, usuarioService.findAll().size());
+		Usuario usuario = new Usuario("Javier", "123456");
+		assertTrue( usuario.getNickName().equals("Javier"));
 		;
 		
 	}
